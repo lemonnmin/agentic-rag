847fd4078a69493fb8132752c527cae626455da3
liujianbing <114247015+lemonnmin@users.noreply.github.com>
2026-04-13 15:52:26 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-client = chromadb.PersistentClient(path="./storage")
-collection = client.get_collection("rag_docs")
-print("向量库文档数量：", collection.count())
-if collection.count() > 0:
-    docs = collection.get()
-    print("第一个文档：", docs["documents"][0][:100])
